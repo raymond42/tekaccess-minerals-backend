09e6af41ba669902d9d8274a3434832923b74f73
Raymond Gakwaya <raymond42.gr@gmail.com>
2020-03-28 17:47:03 +0200

    add product migrations

--- src/config/config.js
+++ src/config/config.js
@@ -12,12 +12,20 @@ module.exports = {
     logging: false,
     seederStorage: "sequelize"
   },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    seederStorage: "sequelize"
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: "DATABASE_URL",
     url: process.env.DATABASE_URL,
-    host: process.env.DB_HOST,
     dialect: "postgres",
     logging: false,
     seederStorage: "sequelize"
