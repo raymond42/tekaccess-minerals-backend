33f27ff70499afa0999a19f2183bb371baa8b6d1
Raymond Gakwaya <raymond42.gr@gmail.com>
2020-03-30 02:51:33 +0200

    feature(user should be able to retrieve products) add retrieve products api

---
+++ src/controllers/products.js
@@ -0,0 +1,33 @@
+import { config } from "dotenv";
+import { Product } from "../sequelize/models";
+
+config();
+
+class ProductsController {
+  static async getAllProducts(req, res) {
+    try {
+      const products = await Product.findAll();
+      res.status(200).json({
+        message: `${products.length} product(s) found`,
+        data: products
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getSingleProduct(req, res) {
+    const productId = req.params.id;
+    try {
+      const product = await Product.findOne({ where: { id: productId } });
+      res.status(200).json({
+        message: `${product.productName} is successful retrieved`,
+        data: product
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default ProductsController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import express from "express";
+import productsRouter from "./products";
 
 const router = express();
+
+router.use("/", productsRouter);
 
 export default router;
